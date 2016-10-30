44c088516ca0e3228c0be593e8098f347374f72b
Wojciech Smolak <wsmolak@gmail.com>
2016-10-30 11:14:54 +0100

    pierwsza zmiana na produkcji

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,10 @@
 //20161030 projekt0 - BigData programowanie wspolbiezne JVM
 public class HelloWorld {
 	public static void main(String[] args) {
+		System.out.println("wersja produkcyjna");
 		System.out.println("Wyswietle zaraz " + args.length + " argumentow");
 		for(int i=0;i<args.length;i++){
 			System.out.println(args[i]);
 		}
 	}
 }
-//nowy komentarz
-//znowu komentarz
