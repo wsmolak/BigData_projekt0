6aa64ad8bf11b10bae0dcf24703e1cbc7da183b4
Wojciech Smolak <xwsmolak@thx17.lab.ii.pw.edu.pl>
2016-10-30 10:15:50 +0100

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//20161030 projekt0 - BigData programowanie wspolbiezne JVM
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Wyswietle zaraz " + args.length + " argumentow");
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+	}
+}
